44d8c05b4b52675b1eafe83ac1f2fa8611f50a05
William Walsh <wmdwalsh0@gmail.com>
2021-07-05 20:37:42 -0400

    commit tree b4966a8ab2ca7e11d1685d526fc786f280c70e1a

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`);
+};
+
+function mondayWork(activity = "go to the office"){
+    return(`This Monday, I will ${activity}.`);
+}
+
+function wrapAdjective(name="*"){
+    // if (name) {
+    //     console.log(name)
+    // }
+    // else {
+    //     name = "*"
+    // }
+    const part1 = "You are"
+    console.log(name + "NAME LINE")
+    return function(x){
+        console.log(x + "X LINE")
+        return `${part1} ${name}${x}${name}!`;
+    }
+}
+wrapAdjective()();
